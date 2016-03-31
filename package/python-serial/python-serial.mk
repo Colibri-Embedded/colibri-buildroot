fb678ccfbe402f00ad3d829dd432fed8eee7f017
Daniel Kesler <kesler.daniel@gmail.com>
2016-03-31 22:27:02 +0200

    - update: pyserial bump version to 3.0.1

--- package/python-serial/python-serial.mk
+++ package/python-serial/python-serial.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_SERIAL_VERSION = 2.7
+PYTHON_SERIAL_VERSION = 3.0.1
 PYTHON_SERIAL_SOURCE = pyserial-$(PYTHON_SERIAL_VERSION).tar.gz
 PYTHON_SERIAL_SITE = http://pypi.python.org/packages/source/p/pyserial
 PYTHON_SERIAL_LICENSE = Python Software Foundation License
 PYTHON_SERIAL_LICENSE_FILES = LICENSE.txt
-PYTHON_SERIAL_SETUP_TYPE = distutils
+PYTHON_SERIAL_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
